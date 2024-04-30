9e6809809dd36a1ac87818a7850fd4cde4dac277
Alexey <Alexey261139@mail.ru>
2024-04-30 13:10:05 +0300

    Раскоментированно применение миграции

--- SpecialLibrary/Program.cs
+++ SpecialLibrary/Program.cs
@@ -11,11 +11,9 @@ namespace SpecialLibrary
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+        {           
             ApplicationConfiguration.Initialize();
-            //SpecialLibraryDbContext.Shared.Database.Migrate();
+            SpecialLibraryDbContext.Shared.Database.Migrate();
             Application.Run(new MainForm());
         }
     }
